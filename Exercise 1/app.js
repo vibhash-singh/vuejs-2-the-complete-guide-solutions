1544652bfae609b83d523c9953d6c467e0bf2b7c
vibhash <vibhash.oirlabs@gmail.com>
2017-08-27 15:11:00 +0530

    Solution to assignment 1

---
+++ Exercise 1/app.js
@@ -0,0 +1,8 @@
+new Vue({
+	el: "#exercise",
+	data: {
+		name: "Vibhash",
+		age: "30",
+		imgLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9N3LXIE3u5u2djj-2iZHj9jECZBWMkKCPB_4_ToO5rWRfgy3K"
+	}
+});
