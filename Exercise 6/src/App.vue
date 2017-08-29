ce6f9253e2915675a1739e8b04a5e736737aac77
vibhash <vibhash.cusat@gmail.com>
2017-08-30 01:50:03 +0530

    Solution to assignment 6

---
+++ Exercise 6/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row">
+          <server-header></server-header>
+        </div>
+        <hr>
+        <div class="row">
+          <servers></servers>
+          <server-details></server-details>
+        </div>
+        <hr>
+        <div class="row">
+          <server-footer></server-footer>
+        </div>
+    </div>
+</template>
+
+<script>
+  import ServerHeader from "./ServerHeader.vue"
+  import ServerFooter from "./ServerFooter.vue"
+  import ServerDetails from "./ServerDetails.vue"
+  import Servers from "./Servers.vue"
+
+  export default {
+    components: {
+      "server-header": ServerHeader,
+      "server-footer": ServerFooter,
+      "server-details": ServerDetails,
+      "servers": Servers
+    }
+  }
+</script>
+
+<style>
+
+</style>
