7be1fc98778e17d62a3ce5b2cbcc5a2df3b1f4fc
vibhash <vibhash.cusat@gmail.com>
2017-08-30 23:15:42 +0530

    wonderful quotes

--- wonderful quotes/src/App.vue
+++ wonderful quotes/src/App.vue
@@ -1,12 +1,35 @@
 <template>
     <div class="container">
-    
+      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+      <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
     </div>
 </template>
 
 <script>
+  import QuoteGrid from "./components/QuoteGrid.vue";
+  import NewQuote from "./components/NewQuote.vue"
+
     export default {
-        
+        data: function() {
+          return {
+            quotes: [
+              'Just a quote to see something'
+            ],
+            maxQuotes: 10
+          }
+        },
+        components: {
+          appNewQuote: NewQuote,
+          appQuoteGrid: QuoteGrid
+        },
+        methods :{
+          newQuote(quote) {
+            this.quotes.push(quote);
+          },
+          deleteQuote(index) {
+            this.quotes.splice(index, 1);
+          }
+        }
     }
 </script>
 
