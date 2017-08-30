b12095f9bc4dfd0373358ba4f5814ff49fef9754
vibhash <vibhash.cusat@gmail.com>
2017-08-30 13:50:23 +0530

    Solution to assignment 7

---
+++ Exercise 7/src/components/Server/Server.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="list-group-item" @click="serverInfo">
+    Server #{{ server.id }}
+  </li>
+</template>
+
+<script>
+  import { eventBus } from "../../main"
+
+  export default {
+    props: ['server'],
+     methods: {
+      serverInfo() {
+        eventBus.$emit('serverInfo', this.server);
+      }
+    },
+  }
+</script>
---
+++ Exercise 7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Select a server</p>
+        <p v-else>Server # {{server.id}} {{server.status}}</p>
+    </div>
+
+</template>
+
+<script>
+  import { eventBus } from "../../main"
+
+  export default {
+    data: function() {
+      return { server : null }
+    },
+    created() {
+      eventBus.$on('serverInfo', (server) => {
+        this.server = server;
+      });
+    }
+  }
+
+</script>
+
+
+<style>
+
+</style>
